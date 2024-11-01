6c56ee1b8072121c0d4f4ae0f7961db1f9dc4344
adrian-zuur <adrian.zuur@globant.com>
2024-11-01 08:57:57 -0500

    add working dash chatbot

--- src/frontend_for_ds/__init__.py
+++ src/frontend_for_ds/__init__.py
@@ -3,8 +3,7 @@ from .streamlit_server import main as streamlit_server_main
 from .plain_demo import main as plain_demo_main
 from .html_server import main as html_server_main
 from .fasthtml_server import main as fasthtml_server_main
-
-# from .dash_server import main as dash_server_main
+from .dash_server import main as dash_server_main
 
 __all__ = [
     "json_server_main",
@@ -12,5 +11,5 @@ __all__ = [
     "plain_demo_main",
     "html_server_main",
     "fasthtml_server_main",
-    # "dash_server_main",
+    "dash_server_main",
 ]
--- src/frontend_for_ds/dash_server.py
+++ src/frontend_for_ds/dash_server.py
@@ -1,164 +1,152 @@
-# import os
-# import time
-# from textwrap import dedent
+# Adapted from https://github.com/plotly/dash-sample-apps/blob/main/apps/dash-gpt3-chatbot/app.py
 
-# import dash
-# import dash_html_components as html
-# import dash_core_components as dcc
-# import dash_bootstrap_components as dbc
-# from dash.dependencies import Input, Output, State
-# from PIL import Image
-# import openai
+from pathlib import Path
+import dash
+from dash import html
+from dash import dcc
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 
-# from frontend_for_ds.backend import get_session_messages, reply
+from frontend_for_ds.backend import get_session_messages, reply
 
 
-# def Header(name, app):
-#     title = html.H1(name, style={"margin-top": 5})
-#     logo = html.Img(
-#         src=app.get_asset_url("dash-logo.png"),
-#         style={"float": "right", "height": 60},
-#     )
-#     return dbc.Row([dbc.Col(title, md=8), dbc.Col(logo, md=4)])
+def Header(name, app):
+    title = html.H1(name, style={"margin-top": 5})
+    # logo = html.Img(
+    #     src=app.get_asset_url("dash-logo.png"),
+    #     style={"float": "right", "height": 60},
+    # )
+    return dbc.Row([dbc.Col(title, md=8)])
 
 
-# def textbox(text, box="AI", name="Philippe"):
-#     text = text.replace(f"{name}:", "").replace("You:", "")
-#     style = {
-#         "max-width": "60%",
-#         "width": "max-content",
-#         "padding": "5px 10px",
-#         "border-radius": 25,
-#         "margin-bottom": 20,
-#     }
+def textbox(text, box="AI", name="AI"):
+    text = text.replace(f"{name}:", "").replace("You:", "")
+    style = {
+        "max-width": "60%",
+        "width": "max-content",
+        "padding": "5px 10px",
+        "border-radius": 25,
+        "margin-bottom": 20,
+    }
 
-#     if box == "user":
-#         style["margin-left"] = "auto"
-#         style["margin-right"] = 0
+    if box == "user":
+        style["margin-left"] = "auto"
+        style["margin-right"] = 0
 
-#         return dbc.Card(
-#             text, style=style, body=True, color="primary", inverse=True
-#         )
+        return dbc.Card(
+            text, style=style, body=True, color="primary", inverse=True
+        )
 
-#     elif box == "AI":
-#         style["margin-left"] = 0
-#         style["margin-right"] = "auto"
+    elif box == "AI":
+        style["margin-left"] = 0
+        style["margin-right"] = "auto"
 
-#         thumbnail = html.Img(
-#             src=app.get_asset_url("Philippe.jpg"),
-#             style={
-#                 "border-radius": 50,
-#                 "height": 36,
-#                 "margin-right": 5,
-#                 "float": "left",
-#             },
-#         )
-#         textbox = dbc.Card(
-#             text, style=style, body=True, color="light", inverse=False
-#         )
+        thumbnail = html.Img(
+            src=app.get_asset_url("ai.jpeg"),
+            style={
+                "border-radius": 50,
+                "height": 36,
+                "margin-right": 5,
+                "float": "left",
+            },
+        )
+        textbox = dbc.Card(
+            text, style=style, body=True, color="light", inverse=False
+        )
 
-#         return html.Div([thumbnail, textbox])
+        return html.Div([thumbnail, textbox])
 
-#     else:
-#         raise ValueError("Incorrect option for `box`.")
-
-
-# description = """
-# Philippe is the principal architect at a condo-development firm in Paris. He lives with his girlfriend of five years in a 2-bedroom condo, with a small dog named Coco. Since the pandemic, his firm has seen a  significant drop in condo requests. As such, he’s been spending less time designing and more time on cooking,  his favorite hobby. He loves to cook international foods, venturing beyond French cuisine. But, he is eager  to get back to architecture and combine his hobby with his occupation. That’s why he’s looking to create a  new design for the kitchens in the company’s current inventory. Can you give him advice on how to do that?
-# """
-
-# # Authentication
-# openai.api_key = os.getenv("OPENAI_KEY")
-
-# # Define app
-# app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-# server = app.server
+    else:
+        raise ValueError("Incorrect option for `box`.")
 
 
-# # Load images
-# IMAGES = {"Philippe": app.get_asset_url("Philippe.jpg")}
+# Define app
+assets_path = Path(__file__).parent.parent.parent / "static"
+app = dash.Dash(
+    __name__,
+    assets_folder=assets_path,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+)
+server = app.server
 
 
-# # Define Layout
-# conversation = html.Div(
-#     html.Div(id="display-conversation"),
-#     style={
-#         "overflow-y": "auto",
-#         "display": "flex",
-#         "height": "calc(90vh - 132px)",
-#         "flex-direction": "column-reverse",
-#     },
-# )
+# Define Layout
+conversation = html.Div(
+    html.Div(id="display-conversation"),
+    style={
+        "overflow-y": "auto",
+        "display": "flex",
+        "height": "calc(90vh - 132px)",
+        "flex-direction": "column-reverse",
+    },
+)
 
-# controls = dbc.InputGroup(
-#     children=[
-#         dbc.Input(
-#             id="user-input", placeholder="Write to the chatbot...", type="text"
-#         ),
-#         dbc.InputGroupAddon(
-#             dbc.Button("Submit", id="submit"), addon_type="append"
-#         ),
-#     ]
-# )
+controls = dbc.InputGroup(
+    children=[
+        dbc.Input(
+            id="user-input", placeholder="Write to the chatbot...", type="text"
+        ),
+        dbc.Button("Submit", id="submit"),
+    ]
+)
 
-# app.layout = dbc.Container(
-#     fluid=False,
-#     children=[
-#         Header("Dash GPT-3 Chatbot", app),
-#         html.Hr(),
-#         dcc.Store(id="store-conversation", data=""),
-#         conversation,
-#         controls,
-#         dbc.Spinner(html.Div(id="loading-component")),
-#     ],
-# )
+app.layout = dbc.Container(
+    fluid=False,
+    children=[
+        Header("Dash GPT Chatbot", app),
+        html.Hr(),
+        dcc.Store(id="store-conversation", data=[]),
+        conversation,
+        controls,
+        dbc.Spinner(html.Div(id="loading-component")),
+    ],
+)
 
 
-# @app.callback(
-#     Output("display-conversation", "children"),
-#     [Input("store-conversation", "data")],
-# )
-# def update_display(chat_history):
-#     return [
-#         textbox(x, box="user") if i % 2 == 0 else textbox(x, box="AI")
-#         for i, x in enumerate(chat_history.split("<split>")[:-1])
-#     ]
+@app.callback(
+    Output("display-conversation", "children"),
+    [Input("store-conversation", "data")],
+)
+def update_display(chat_history):
+    return [
+        (
+            textbox(message["content"], box="user")
+            if message["role"] == "user"
+            else textbox(message["content"], box="AI")
+        )
+        for message in chat_history[1:]
+    ]
 
 
-# @app.callback(
-#     Output("user-input", "value"),
-#     [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
-# )
-# def clear_input(n_clicks, n_submit):
-#     return ""
+@app.callback(
+    Output("user-input", "value"),
+    [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
+)
+def clear_input(n_clicks, n_submit):
+    return ""
 
 
-# @app.callback(
-#     [
-#         Output("store-conversation", "data"),
-#         Output("loading-component", "children"),
-#     ],
-#     [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
-#     [State("user-input", "value"), State("store-conversation", "data")],
-# )
-# def run_chatbot(n_clicks, n_submit, user_input, chat_history):
-#     if n_clicks == 0 and n_submit is None:
-#         return "", None
+@app.callback(
+    [
+        Output("store-conversation", "data"),
+        Output("loading-component", "children"),
+    ],
+    [Input("submit", "n_clicks"), Input("user-input", "n_submit")],
+    [State("user-input", "value"), State("store-conversation", "data")],
+)
+def run_chatbot(n_clicks, n_submit, user_input, chat_history):
+    if n_clicks == 0 and n_submit is None:
+        return [], None
 
-#     if user_input is None or user_input == "":
-#         return chat_history, None
+    if user_input is None or user_input == "":
+        return chat_history, None
 
-#     session_id = 1
+    session_id = 1
+    _ = reply(session_id, user_input)
+    chat_history = get_session_messages(session_id)
 
-#     _ = reply(session_id, user_input)
-#     chat_history = get_session_messages(session_id)
-
-#     return chat_history, None
-
-
-# def main():
-#     app.run_server(debug=False)
+    return chat_history, None
 
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    app.run_server(debug=True)
